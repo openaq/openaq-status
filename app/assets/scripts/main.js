09f3da746ff8664bd7b28b62c37acbc35de4a18e
Russ Biggs <russbiggs@gmail.com>
2021-12-22 16:17:39 -0700

    initial hugo build

--- app/assets/scripts/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-import 'babel-polyfill';
-import React from 'react';
-import { render } from 'react-dom';
-
-import App from './views/app';
-
-render(<App />, document.querySelector('#app-container'));
