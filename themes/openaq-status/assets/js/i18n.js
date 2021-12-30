7b85a1db83037bec4d2d9c47dfb94ed7d313eb93
Russ Biggs <russbiggs@gmail.com>
2021-12-30 11:36:27 -0700

    initial i18n build and mobile layout updates

---
+++ themes/openaq-status/assets/js/i18n.js
@@ -0,0 +1,25 @@
+const langs = {
+    "en": {
+        "allServicesOperational":"All Services Operational",
+        "intermittentOutages":"Intermittent Outages",
+        "allServicesDown":"All Services Down",
+        "months": ["January","February","March","April","May","June","July",
+        "August","September","October","November","December"]
+    },
+    "es": {
+        "allServicesOperational":"Todos los servicios operativos",
+        "intermittentOutages":"Intermittent Outages",
+        "allServicesDown":"All Services Down",
+        "months": ["enero","febrero","marzo","abril","mayo","junio","julio",
+        "agosto","septiembre","octubre","noviembre","diciembre"]
+    },
+    "it": {
+        "allServicesOperational":"Tutti i servizi operativi",
+        "intermittentOutages":"Intermittent Outages",
+        "allServicesDown":"All Services Down",
+        "months": ["gennaio","febbraio","marzo","aprile","maggio","giugno","luglio",
+        "agosto","settembre","ottobre","novembre","dicembre"]
+    }
+}
+
+export default langs;
